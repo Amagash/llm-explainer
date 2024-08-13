cad907efa71083934ac5861e18082dc83ece2f1c
Amagash <tiffanysouterre@gmail.com>
2024-08-13 16:24:09 +0200

    update streamlit

--- streamlit.py
+++ streamlit.py
@@ -15,24 +15,34 @@ print(type(words))
 print(words)
 
 model = gensim.downloader.load('glove-wiki-gigaword-50')
-print(words[0], model[words[0]])
-print(words[1], model[words[1]])
 
 vector_1 = model[words[0]][0:3]
 vector_2 = model[words[1]][0:3]
+vector_3 = model[words[2]][0:3]
+vector_4 = model[words[3]][0:3]
+vector_5 = model[words[4]][0:3]
+vector_6 = model[words[5]][0:3]
 
 fig = plt.figure(figsize = (10,10))
 ax = plt.axes(projection='3d')
 
 ax.text(vector_1[0], vector_1[1], vector_1[2], words[0])
 ax.text(vector_2[0], vector_2[1], vector_2[2], words[1])
+ax.text(vector_3[0], vector_3[1], vector_3[2], words[2])
+ax.text(vector_4[0], vector_4[1], vector_4[2], words[3])
+ax.text(vector_5[0], vector_5[1], vector_5[2], words[4])
+ax.text(vector_6[0], vector_6[1], vector_6[2], words[5])
 
 soa = np.array([[0, 0, 0, vector_1[0], vector_1[1], vector_1[2]], 
-                [0, 0, 0, vector_2[0], vector_2[1], vector_2[2]]
+                [0, 0, 0, vector_2[0], vector_2[1], vector_2[2]],
+                [0, 0, 0, vector_3[0], vector_3[1], vector_3[2]],
+                [0, 0, 0, vector_4[0], vector_4[1], vector_4[2]],
+                [0, 0, 0, vector_5[0], vector_5[1], vector_5[2]],
+                [0, 0, 0, vector_6[0], vector_6[1], vector_6[2]],
                 ])
 
 X, Y, Z, U, V, W = zip(*soa)
-ax.quiver(X, Y, Z, U, V, W, color=['r','b'])
+ax.quiver(X, Y, Z, U, V, W, color=['r','b','g','y','m','c'])
 ax.set_xlim([-1.5, 1.5])
 ax.set_ylim([-1.5, 1.5])
 ax.set_zlim([-1.5, 1.5])
